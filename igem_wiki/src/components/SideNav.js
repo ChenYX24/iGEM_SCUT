70e2b2fd071c35e161b17bb7e146c2fc7b127afb
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-01-13 17:23:56 +0800

    完善滚动监听

--- igem_wiki/src/components/SideNav.js
+++ igem_wiki/src/components/SideNav.js
@@ -1,33 +1,61 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
 import { useState } from 'react'
 import SideNavCss from "../css/SideNav.module.css"
 import $ from '../js/jquery'
 function SideNav (props) {
-  const [isActive, setActive] = useState(true)
-  //跳转
-  const clickHandler = (e) => {
-    const target = $(e.currentTarget).text()
-    $("html, body").animate({
-      scrollTop: $(`#${target}`).offset().top
-    }, { duration: 500, easing: "swing" })
-    setActive(!isActive)
-    return false
+const [data, setData] = useState(props.data)
+const [isClick, setClick] = useState(false)
+//跳转
+const clickHandler = (e, id) => {
+  setClick(true)
+  const target = $(e.currentTarget).text()
+  $("html, body").animate({
+    scrollTop: $(`#${target}`).offset().top
+  }, { duration: 500, easing: "swing" })
+    .promise()
+    .done(function () {
+      setClick(false)
+    })
+  let newData = [...data]
+  if (newData[id].isActive === false) {
+    newData.map(item => {
+      if (item.isActive === true) {
+        item.isActive = false
+      }
+    })
+    newData[id].isActive = !newData[id].isActive
+    setData(newData)
   }
-  return (
-    <>
-      <div className={SideNavCss.SideNav}>
-        <div className={isActive ? `${SideNavCss.button}` : `${SideNavCss.button}`} onClick={clickHandler}>
-          Background
-        </div>
-        <div className={`${SideNavCss.button}`} onClick={clickHandler}>
-          Overview
-        </div>
-        <div className={SideNavCss.button} onClick={clickHandler}>
-          References
-        </div>
-      </div>
-    </>
-  )
+
 }
+//监听
+$(window).scroll(function () {
+  if (!isClick) {
+    let newData = [...data]
+    var docScrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    data.map(item => {
+
+      if (docScrollTop > $(`#${item.text}`).offset().top - 10) {
+        newData.map(item2 => {
+          item2.isActive = false
+        })
+        newData[item.id].isActive = true
+      }
+    })
+    setData(newData)
+  }
+})
+
+return (
+  <>
+    <div className={SideNavCss.SideNav}>
+      {data.map(item => (
+        <div key={item.id} className={`${SideNavCss.button} ${item.isActive ? SideNavCss.active : ""}`} onClick={(e) => clickHandler(e, item.id)}>{item.text}</div>
+      ))}
+    </div>
+  </>
+)}
+
 export default SideNav
--- igem_wiki/src/pages/Home.js
+++ igem_wiki/src/pages/Home.js
@@ -3,17 +3,17 @@ import HomeMain from "../components/HomeMain"
 import SideNav from "../components/SideNav"
 import HomeCss from "../css/Home.module.css"
 class Home extends React.Component {
+  static NavData = [{ id: 0, text: 'Background', isActive: true }, { id: 1, text: 'Overview', isActive: false }, { id: 2, text: 'References', isActive: false },]
   render () {
     return (
       <>
-        <SideNav />
+        <SideNav data={Home.NavData} />
         <div className={HomeCss.HomeFrame}>
 
           <HomeMain />
           <div className={HomeCss.right}></div>
         </div>
       </>
-
     )
   }
 
