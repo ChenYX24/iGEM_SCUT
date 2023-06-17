e782fde965b499d31e902a8c1f6798cbe5042560
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2023-06-17 20:24:13 +0800

    广东测试

--- igem_wiki/src/pages/Home.js
+++ igem_wiki/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react"
 import GlassCard from "../components/GlassCard"
+import GuangDong from "../components/GuangDong"
 import HomeMain from "../components/HomeMain"
 import Loading from "../components/Loading"
 import NavBar from "../components/NavBar"
@@ -7,7 +8,6 @@ import ProgressBar from "../components/ProgressBar"
 import SideNav from "../components/SideNav"
 import Wave from "../components/Wave"
 import HomeCss from "../css/Home.module.css"
-
 
 class Home extends React.Component {
   static NavData = [{ id: 0, text: 'Background', isActive: true }, { id: 1, text: 'Overview', isActive: false }, { id: 2, text: 'References', isActive: false },]
@@ -44,7 +44,7 @@ class Home extends React.Component {
               <GlassCard />
             </div>
             <ProgressBar />
-
+            <GuangDong />
             <Wave />
           </>
 
